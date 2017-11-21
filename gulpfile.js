108aaf2560d74d7579084d4875cf4df07ea854e7
見上 <kaori_kenjo@fablic.co.jp>
2017-11-21 11:58:48 +0900

    fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 var cleanCss = require('gulp-clean-css');
 gulp.task('sass',function(){
-    gulp.src(['css/src/all.scss'])
+    gulp.src(['css/src/*.scss'])
+        .pipe(plumber({
+            handleError: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))
         .pipe(sass({
             style : 'expanded'
         }))
+        .pipe(gulp.dest('css/dist'))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(cleanCss())
         .pipe(gulp.dest('css/dist'))
 });
 gulp.task('default',function(){
